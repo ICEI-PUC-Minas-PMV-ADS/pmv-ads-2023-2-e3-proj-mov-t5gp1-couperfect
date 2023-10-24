085ef7fb2140f9c31828a1ab622a07281c95cba2
LAPTOP-P1LBD27N\Littlebone <luizeduardodejes@gmail.com>
2023-10-24 20:12:21 -0300

    23 - Adicionando tela de login

--- src/couperfectclient/App.tsx
+++ src/couperfectclient/App.tsx
@@ -6,7 +6,8 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import LoginScreen from './Pages/LoginScreen';
+import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -29,7 +30,7 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): JSX.Element {
+function Section({ children, title }: SectionProps): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -76,19 +77,7 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
+            < LoginScreen />
           <LearnMoreLinks />
         </View>
       </ScrollView>
---
+++ src/couperfectclient/Pages/LoginScreen.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import InputField from '../components/InputField';
+import LoginButton from '../components/LoginButton';
+
+
+const LoginScreen: React.FC = () => {
+  const handleLogin = () => {
+    console.log("Handle login")
+    // Implemente a lógica de login aqui
+  };
+
+  return (
+    <View style={styles.container}>
+      <InputField placeholder="Email" />
+      <InputField placeholder="Senha" secureTextEntry />
+      <LoginButton onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+});
+
+export default LoginScreen;
